63e03d841072841d36e7cef5bb78c759e7381b39
Aleksandr_OZ <korvet95@mail.ru>
2022-04-20 23:25:08 +0300

    init

---
+++ lesson1/main.js
@@ -0,0 +1,21 @@
+// const products = [
+//   { id: 1, title: "Notebook", price: 1000 },
+//   { id: 2, title: "Mouse", price: 100 },
+//   { id: 3, title: "Keyboard", price: 250 },
+//   { id: 4, title: "Gamepad", price: 150 },
+// ];
+
+// // Функция с дефолтными параметрами
+// const getProductHTMLString = (title = null, price = null) =>
+//   `<div class="product-item">
+//       <h3>${title}</h3>
+//       <p>${price}</p>
+//       <button class="by-btn">Добавить</button>
+//     </div>`;
+
+// const el = document.querySelector(".products");
+
+// // Вставка в html
+// products.forEach((prod) =>
+//   el.insertAdjacentHTML("beforeend", getProductHTMLString(prod.title, prod.price))
+// );
