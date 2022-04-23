be739c1df943d847bad5d5b210aadc2ec64d93da
Aleksandr_OZ <korvet95@mail.ru>
2022-04-23 18:12:24 +0300

    lesson2-ready

--- lesson2/project/js/main.js
+++ lesson2/project/js/main.js
@@ -1,20 +1,29 @@
 class ProductList {
-  constructor(container = '.products') {
+  constructor(container = ".products") {
     this.container = document.querySelector(container);
     this.goods = [];
     this.productObjects = [];
 
+    this.sum = 0;
+
     this.fetchGoods();
+    this.getSum();
     this.render();
   }
 
   fetchGoods() {
     this.goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
+      { id: 1, title: "Notebook", price: 20000 },
+      { id: 2, title: "Mouse", price: 1500 },
+      { id: 3, title: "Keyboard", price: 5000 },
+      { id: 4, title: "Gamepad", price: 4500 },
     ];
+  }
+
+  // Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
+  getSum() {
+    if (this.goods?.length) this.goods.forEach((el) => (this.sum += el.price));
+    console.log(this.sum);
   }
 
   render() {
@@ -23,13 +32,13 @@ class ProductList {
       console.log(productObject);
       this.productObjects.push(productObject);
 
-      this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+      this.container.insertAdjacentHTML("beforeend", productObject.getHTMLString());
     }
   }
 }
 
 class ProductItem {
-  constructor(product, img='https://via.placeholder.com/200x150') {
+  constructor(product, img = "https://via.placeholder.com/200x150") {
     this.id = product.id;
     this.title = product.title;
     this.price = product.price;
@@ -50,27 +59,25 @@ class ProductItem {
 
 new ProductList();
 
-// const products = [
-//   {id: 1, title: 'Notebook', price: 20000},
-//   {id: 2, title: 'Mouse', price: 1500},
-//   {id: 3, title: 'Keyboard', price: 5000},
-//   {id: 4, title: 'Gamepad', price: 4500},
-// ];
-//
-// const renderProduct = (item, img='https://via.placeholder.com/200x150') => `<div class="product-item">
-//               <img src="${img}" alt="Some img">
-//               <div class="desc">
-//                   <h3>${item.title}</h3>
-//                   <p>${item.price} \u20bd</p>
-//                   <button class="buy-btn">Купить</button>
-//               </div>
-//           </div>`;
-//
-// const renderProducts = list => {
-//   document
-//       .querySelector('.products')
-//       .insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
-// };
-//
-// renderProducts(products);
-//
+/*Добавьте пустые классы для Корзины товаров и Элемент корзины товаров. Продумайте,
+какие методы понадобятся для работы с этими сущностями. */
+class Cart {
+  constructor(cart = ".cart") {
+    this.cart = document.querySelector(cart);
+    this.products = [];
+    this.buy();
+    this.addToCart();
+    this.deleteFromCart();
+    this.openCart();
+    this.closeCart();
+    this.render();
+  }
+}
+
+/*Добавьте пустые классы для Корзины товаров и Элемент корзины товаров. Продумайте,
+какие методы понадобятся для работы с этими сущностями. */
+class CartProduct extends ProductItem {
+  constructor(id, title, price, img) {
+    super(id, title, price, img);
+  }
+}
